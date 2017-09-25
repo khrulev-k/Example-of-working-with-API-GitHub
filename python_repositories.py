9047cecab5b8d50b6a1c1bd3a73f3b6f287e6aa3
Khrulev Kirill <khrulev.k@yandex.ru>
2017-09-25 13:54:39 +0300

    First commit python_repositories.py
    The program reads out data on the number of projects of Python placed on GitHub now and also information on the most popular repositories of Python.

---
+++ python_repositories.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+
+# TODO process the response status
+print("Status code:", r.status_code)
+# 200 — successful response
+
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+repo_dict = repo_dicts[0]
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
