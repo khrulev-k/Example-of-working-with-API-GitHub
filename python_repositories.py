2a0f06d7ce58e4a3279f6712f6aa9dbf248c5636
Khrulev Kirill <khrulev.k@yandex.ru>
2017-09-25 15:34:56 +0300

    Completion of charts.
    Hints with the description and active references of repositories are added.

--- python_repositories.py
+++ python_repositories.py
@@ -27,14 +27,29 @@ print('Created:', repo_dict['created_at'])
 print('Updated:', repo_dict['updated_at'])
 print('Description:', repo_dict['description'])
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
 
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 16
+my_config.truncate_label = 14
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('./img/python_repositories.svg')
